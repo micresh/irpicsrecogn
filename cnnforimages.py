81de3297ff264d16402e95d63b942bc0689866c6
micresh <micresh@gmail.com>
2016-12-03 18:53:56 +0300

    fix bug with unproval shape at input layer

--- cnnforimages.py
+++ cnnforimages.py
@@ -6,7 +6,7 @@ import numpy as np
 outputs = 20
 n = 36864
 
-inp = Input(shape=n)
+inp = Input(shape=(n,)) # 1d vector as inputdata
 hidden_1 = Dense(0.1*n, activation='tanh')(inp)
 out = Dense(outputs,activation='sigmoid')(hidden_1)
 model = Model(input=inp, output = out)
