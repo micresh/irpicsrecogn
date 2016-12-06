30a44596e3a39216d689dde3e2db1ac28d333577
micresh <micresh@gmail.com>
2016-12-06 23:17:07 +0300

    update main file with new expierence, added function test_network, where we create a framework

--- cnnforimages.py
+++ cnnforimages.py
@@ -1,16 +1,44 @@
 from keras.models import Model
 from keras.layers import Input, Dense
-
+from tifffile import *
 import numpy as np
 
-outputs = 20
-n = 36864
+window_height = 576
+window_width = 200
+image_step = window_width
+outputs = 10
 
-inp = Input(shape=(n,)) # 1d vector as inputdata
-hidden_1 = Dense(0.1*n, activation='tanh')(inp)
-out = Dense(outputs,activation='sigmoid')(hidden_1)
-model = Model(input=inp, output = out)
-model.compile(optimizer='rmsprop', 
-              loss = 'mean_absolute_error',
-              metrics=['mse'])
 
+def test_network():
+    inp = Input(shape=(window_height, window_width,))  # 3d vector as inputdata
+    hidden_1 = Dense(0.1*n, activation='tanh')(inp)
+    out = Dense(outputs, activation='sigmoid')(hidden_1)
+    model = Model(input=inp, output=out)
+    model.compile(optimizer='rmsprop',
+                  loss='mse',
+                  metrics=['accuracy'])
+    return model.evaluate()
+
+
+with open('objects.csv', 'r') as f:
+    reader = csv.reader(f, delimiter=',')
+    objects_coor = []
+    for row in reader:
+        t = [int(row[0]), int(row[1]), int(row[2])]
+        objects_coor.append(t)
+
+for picnum in range(1, 1001):
+    s = "/home/micresh/traindata/Train1/" + str(picnum) + '.tif'
+    tiff = imread(s)
+    images_count = 16000/image_step
+    objects_on_pic = []
+    for i in range(len(objects_coor)):
+        if objects_coor[i][0] == picnum:
+            objects_on_pic.append([objects_on_pic[i][1], objects_on_pic[i][2]])
+    out_data = np.zeros((images_count, 8))
+    in_data = np.zeros(((images_count, window_height, window_width)))
+    for step in range(images_count):
+        for y_cor in range(window_height):
+            for x in range(window_width):
+                in_data[step][y_cor][x_cor] = tiff[c][n][y_cor][x_cor + image_step * step]
+        # out_data - need to create a method to generate out data 
